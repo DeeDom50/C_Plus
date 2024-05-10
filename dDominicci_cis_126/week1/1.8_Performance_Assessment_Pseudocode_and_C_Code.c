d46064f0e1f5c254fc19e7d7ab05083a396d053c
Devante <ddominicci50@gmail.com>
2024-05-10 12:36:17 -0500

    finished C code for class

--- dDominicci_cis_126/week1/1.8_Performance_Assessment_Pseudocode_and_C_Code.c
+++ dDominicci_cis_126/week1/1.8_Performance_Assessment_Pseudocode_and_C_Code.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
 
 int main() {
-    float taxRate = .08;
-    float salesAmount;
-    float salesTax = salesAmount * taxRate;
-    float totalSales = salesAmount + salesTax;;
+    float taxRate = 0.08; // Tax rate
+    float salesAmount; // Variable to store sales amount input by the user
+    float salesTax; // Variable to store calculated sales tax
+    float totalSales; // Variable to store total sales amount
 
-    printf("Enter the sales amount: ");
-    scanf("%f", &salesAmount);
+    printf("Enter the sales amount:\n");
+    scanf("%f", &salesAmount); // Read sales amount from user input
 
-    printf("Here is the sales tax: %.2f\n", salesTax);
-    printf("Total Sales Amount: %.2f\n", totalSales);
+    salesTax = salesAmount * taxRate; // Calculate sales tax based on the input sales amount
+    totalSales = salesAmount + salesTax; // Calculate total sales including tax
 
-    return 0;
+    printf("Here is the sales tax: %.2f\n", salesTax); // Print the sales tax
+    printf("Total Sales Amount: %.2f\n", totalSales); // Print the total sales amount
+
+    return 0; 
 }
